0149097b970992e8c35256bf632af096bb440f07
Rupsa Roy <rupsaroy305@gmail.com>
2026-07-21 10:46:45 +0530

    Solve adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    w=np.asarray(w,float)
+    g=np.asarray(g,float)
+    G=np.asarray(G,float)
+    G=G+g*g
+    w=w-lr*g/np.sqrt(G+eps)
+    return w,G
