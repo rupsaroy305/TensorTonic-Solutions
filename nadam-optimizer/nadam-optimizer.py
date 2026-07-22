06469089cc92f08beab5e9bbc9080028fbc1613b
Rupsa Roy <rupsaroy305@gmail.com>
2026-07-22 22:42:04 +0530

    Solve nadam-optimizer

---
+++ nadam-optimizer/nadam-optimizer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def nadam_step(w, m, v, grad, lr=0.002, beta1=0.9, beta2=0.999, eps=1e-8):
+    w=np.asarray(w,float)
+    m=np.asarray(m,float)
+    v=np.asarray(v,float)
+    grad=np.asarray(grad,float)
+    m=beta1*m+(1-beta1)*grad
+    v=beta2*v+(1-beta2)*grad**2
+    w=w-lr*(beta1*m+(1-beta1)*grad)/(np.sqrt(v)+eps)
+    return w,m,v
