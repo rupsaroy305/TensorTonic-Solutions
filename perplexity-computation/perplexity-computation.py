775ab569a651055cd52a82f41d9d5799c094aaa8
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-03 10:55:46 +0530

    Solve perplexity-computation

--- perplexity-computation/perplexity-computation.py
+++ perplexity-computation/perplexity-computation.py
@@ -1,5 +1,5 @@
 import numpy as np
-def perplexity(prob_distributions, actual_tokens):
-    p=np.asarray(prob_distributions,dtype=float)
-    t=np.asarray(actual_tokens)
-    return float(np.exp(-np.mean(np.log(p[np.arange(len(t)),t]))))
+
+def perplexity(prob_distributions,actual_tokens):
+    p=np.array([prob_distributions[i][actual_tokens[i]] for i in range(len(actual_tokens))],float)
+    return float(np.exp(-np.mean(np.log(p))))
