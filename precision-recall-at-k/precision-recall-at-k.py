3a6f59161163b5c9f5801801d59a2cc33226f2cb
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-06 19:55:00 +0530

    Solve precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -1,10 +1,3 @@
-def precision_recall_at_k(recommended,relevant,k):
-    top=recommended[:k]
-    rel=set(relevant)
-    hits=0
-    for x in top:
-        if x in rel:
-            hits+=1
-    precision=hits/k if k>0 else 0.0
-    recall=hits/len(rel) if len(rel)>0 else 0.0
-    return [float(precision),float(recall)]
+def precision_recall_at_k(recommended, relevant, k):
+    h=sum(i in set(relevant) for i in recommended[:k])
+    return [h/k,h/len(relevant)]
