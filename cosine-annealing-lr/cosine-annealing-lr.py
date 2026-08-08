24f6454cbb8276cd21c382585f2d76acb418245e
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-08 10:37:50 +0530

    Solve cosine-annealing-lr

--- cosine-annealing-lr/cosine-annealing-lr.py
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -1,3 +1,3 @@
-import math
 def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
-    return float(min_lr+0.5*(base_lr-min_lr)*(1+math.cos(math.pi*current_step/total_steps)))
+    import numpy as np
+    return float(min_lr+0.5*(base_lr-min_lr)*(1+np.cos(np.pi*current_step/total_steps)))
