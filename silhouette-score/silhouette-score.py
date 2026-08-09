7a34f3473753db8eae23fdd48fa462c115d7f938
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-09 10:47:13 +0530

    Solve silhouette-score

--- silhouette-score/silhouette-score.py
+++ silhouette-score/silhouette-score.py
@@ -1,23 +1,10 @@
 import numpy as np
 
 def silhouette_score(X, labels):
-    X=np.asarray(X,float)
-    labels=np.asarray(labels)
-    d=np.sqrt(((X[:,None]-X[None,:])**2).sum(axis=2))
-    s=[]
-    for i in range(len(X)):
-        same=labels==labels[i]
-        same[i]=False
-        if np.any(same):
-            a=d[i,same].mean()
-        else:
-            a=0.0
-        b=np.inf
-        for c in np.unique(labels):
-            if c==labels[i]:
-                continue
-            m=labels==c
-            if np.any(m):
-                b=min(b,d[i,m].mean())
-        s.append((b-a)/max(a,b) if max(a,b)>0 else 0.0)
-    return float(np.mean(s))
+    import numpy as np
+    X=np.asarray(X); labels=np.asarray(labels)
+    d=np.linalg.norm(X[:,None]-X[None,:],axis=2)
+    u=np.unique(labels)
+    a=np.array([d[i,labels==labels[i]].sum()/(sum(labels==labels[i])-1) if sum(labels==labels[i])>1 else 0 for i in range(len(X))])
+    b=np.array([min(d[i,labels==c].mean() for c in u if c!=labels[i]) for i in range(len(X))])
+    return float(np.mean((b-a)/np.maximum(a,b)))
