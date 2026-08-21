8ab10c961a07b9730f92ae0d0b80254cc926882b
Rupsa Roy <rupsaroy305@gmail.com>
2026-08-21 12:32:07 +0530

    Solve metrics-f1-micro

--- metrics-f1-micro/metrics-f1-micro.py
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -1,15 +1,8 @@
-def f1_micro(y_true,y_pred):
-    tp=fp=fn=0
-
-    for t,p in zip(y_true,y_pred):
-        if t==p:
-            tp+=1
-        else:
-            fp+=1
-            fn+=1
-
-    d=2*tp+fp+fn
-    if d==0:
+def f1_micro(y_true: list[int], y_pred: list[int]) -> float:
+    tp=sum(t==p for t,p in zip(y_true,y_pred))
+    total=len(y_true)
+    fp=total-tp
+    fn=total-tp
+    if 2*tp+fp+fn==0:
         return 0.0
-
-    return float((2*tp)/d)
+    return round(2*tp/(2*tp+fp+fn),4)
