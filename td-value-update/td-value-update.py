0203bdf8ec3c7651693d5a195e8b5fd84ae6b752
Rupsa Roy <rupsaroy305@gmail.com>
2026-09-17 17:13:54 +0530

    Solve td-value-update

--- td-value-update/td-value-update.py
+++ td-value-update/td-value-update.py
@@ -1,8 +1,7 @@
 import numpy as np
 
-def td_value_update(V, s, r, s_next, alpha, gamma):
-    V_new=np.array(V,dtype=float,copy=True)
-    target=r+gamma*V_new[s_next]
-    delta=target-V_new[s]
-    V_new[s]+=alpha*delta
+def td_value_update(V: list, s: int, r: float, s_next: int, alpha: float, gamma: float) -> np.ndarray:
+    V_new = np.array(V, dtype=float, copy=True)
+    td_error = r + gamma * V_new[s_next] - V_new[s]
+    V_new[s] += alpha * td_error
     return V_new
